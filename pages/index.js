8fde476030f2c5f0fc06ea50b322e69352a47394
Ye Joo Park <subwaymatch@gmail.com>
2021-02-16 13:56:26 -0600

    Add sql.js minimal example

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,89 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState, useEffect } from "react";
+import initSqlJs from "sql.js";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  return (
+export default function SqlJsPage() {
+  const [db, setDb] = useState(null);
+  const [error, setError] = useState(null);
+  const [execResults, setExecResults] = useState(null);
+
+  useEffect(() => {
+    initSqlJs({
+      // Fetch sql.js wasm file from CDN
+      // This way, we don't need to deal with webpack
+      locateFile: (file) =>
+        `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.4.0/dist/${file}`,
+    })
+      .then((SQL) => setDb(new SQL.Database()))
+      .catch((err) => setError(err));
+  }, []);
+
+  const exec = (sql) => {
+    try {
+      const results = db.exec(sql);
+      setExecResults(results);
+      setError(null);
+    } catch (err) {
+      setExecResults(null);
+      setError(err);
+    }
+  };
+
+  /**
+   * Renders a single value of the array returned by db.exec(...) as a table
+   */
+  const ResultTable = ({ columns, values }) => {
+    return (
+      <table>
+        <thead>
+          <tr>
+            {columns.map((columnName) => (
+              <td key={columnName}>{columnName}</td>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {values.map((
+            row, // values is an array of arrays representing the results of the query
+            rowIndex
+          ) => (
+            <tr key={rowIndex}>
+              {row.map((value, cellIndex) => (
+                <td key={cellIndex}>{value}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+
+  return db ? (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <h1>Next.js SQL interpreter</h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <textarea
+        onChange={(e) => exec(e.target.value)}
+        placeholder='Enter some SQL. No inspiration ? Try "select sqlite_version()"'
+        className={styles.codeBox}
+      />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <pre className={styles.error}>{(error || "").toString()}</pre>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <pre>
+        {execResults
+          ? execResults.map((execResult, rIndex) => (
+              <ResultTable
+                key={rIndex}
+                columns={execResult.columns}
+                values={execResult.values}
+              />
+            ))
+          : ""}
+      </pre>
     </div>
-  )
+  ) : (
+    <pre>Loading...</pre>
+  );
 }
